99d93087b6e8ec9bca62272cbd9c71ed3eb4dcf2
ahmed-sala <ahmeds66210@gmail.com>
2024-09-18 15:19:37 +0300

    make files structure.

---
+++ lib/core/constant/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color grey = Color(0xFF979797);
+  static const Color primaryColor = Color(0xFF281C9D);
+  static const Color backgroundColor = Color(0xFFF8F9FD);
+  static const Color black = Color(0xFF000000);
+  static const Color containerGreyColor = Color(0xFFDFDFDF);
+}
---
+++ lib/core/constant/imageAssets.dart
@@ -0,0 +1,13 @@
+class AppImageassets {
+  static const String rootImages = 'assets/images';
+  static const String onboardingImageOne = '$rootImages/one.png';
+  static const String onboardingImageTwo = '$rootImages/two.png';
+  static const String onboardingImageThree = '$rootImages/three.png';
+  static const String logo = '$rootImages/logo.png';
+  static const String login = '$rootImages/login.png';
+  static const String signup = '$rootImages/signup.png';
+  static const String googleIcon = '$rootImages/google_icon.png';
+  static const String resetPasswordImage = '$rootImages/forget_password.png';
+  static const String successTransferImage = '$rootImages/success_transfer.png';
+}
+// assets/images/onboardingfour.PNG
